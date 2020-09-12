81dd94659951d903d6069f5c5339b0b9b429098e
Devil7DK <dineshthangavel47@gmail.com>
2020-09-12 14:35:39 +0530

    Implement dynamically creating header links

--- src/components/AppNavBar/DesktopNavBar.tsx
+++ src/components/AppNavBar/DesktopNavBar.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import { Image, Menu } from 'semantic-ui-react';
+import React, { ReactNode } from 'react';
+import { History } from 'history';
+import { useHistory } from 'react-router';
+import { Image, Menu, Icon } from 'semantic-ui-react';
+import { SemanticICONS } from 'semantic-ui-react/dist/commonjs/generic';
+import { NavLinks } from './NavLinks';
 
 export const DesktopNavBar: React.FC = () => {
+    const history: History = useHistory();
     return (
-        <Menu fixed='top' borderless pointing>
+        <Menu fixed='top' borderless>
             <Menu.Item as='a' header><Image style={{ marginRight: '1.5em' }} src='/logo.png' size='mini' />C. DHANABAL</Menu.Item>
             <Menu.Menu position='right'>
-                <Menu.Item as='a' header >Home</Menu.Item>
+                {
+                    NavLinks.map<ReactNode>(link =>
+                        <Menu.Item active={history.location.pathname.startsWith(link.pathname)} onClick={() => history.push(link.pathname)} link>
+                            <Icon name={link.icon as SemanticICONS} />{link.text}
+                        </Menu.Item>
+                    )
+                }
             </Menu.Menu>
         </Menu>
     );
---
+++ src/components/AppNavBar/NavLinks.ts
@@ -0,0 +1,5 @@
+import { SemanticICONS } from "semantic-ui-react/dist/commonjs/generic";
+
+export const NavLinks:{pathname:string, icon: SemanticICONS, text: string}[] = [
+    {pathname: '/home', icon: 'home', text: 'Home'}
+];
